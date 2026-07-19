8e39e96561d21355bad0265dfc3ca9fb212739cd
basanti <basant123bsnt@gmail.com>
2026-07-19 13:28:58 +0545

    feat(controllers): add AccountController with Login GET action

---
+++ SaaSInventoryManagement/Controllers/AccountController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SaaSInventoryManagement.ViewModels.Account;
+
+namespace SaaSInventoryManagement.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login(string? returnUrl = null)
+        {
+            if (User.Identity?.IsAuthenticated == true)
+                return RedirectToAction("Index", "Home");
+
+            ViewData["ReturnUrl"] = returnUrl;
+            return View(new LoginViewModel());
+        }
+    }
+}
