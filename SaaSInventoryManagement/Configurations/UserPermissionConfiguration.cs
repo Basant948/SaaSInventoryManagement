156959fd827993f54de2fc8e31962202fd490bfc
basanti <basant123bsnt@gmail.com>
2026-07-19 10:26:45 +0545

    feat(config): add UserPermissionConfiguration with composite unique index and cascade deletes

---
+++ SaaSInventoryManagement/Configurations/UserPermissionConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SaaSInventoryManagement.Models;
+
+namespace SaaSInventoryManagement.Configurations
+{
+    public class UserPermissionConfiguration : IEntityTypeConfiguration<UserPermission>
+    {
+        public void Configure(EntityTypeBuilder<UserPermission> builder)
+        {
+            builder.HasIndex(up => new { up.UserId, up.PermissionId }).IsUnique();
+
+            builder.HasOne(up => up.User)
+                .WithMany()
+                .HasForeignKey(up => up.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasOne(up => up.Permission)
+                .WithMany()
+                .HasForeignKey(up => up.PermissionId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
