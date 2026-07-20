0b4374c34d13cbf9df75cc462cdbbcadbf576f3b
basanti <basant123bsnt@gmail.com>
2026-07-20 08:13:35 +0545

    chore: Add migration for Ispermission flag

---
+++ SaaSInventoryManagement/Migrations/20260720022423_AddIsPermissionManagedToApplicationUser.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SaaSInventoryManagement.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIsPermissionManagedToApplicationUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsPermissionManaged",
+                table: "AspNetUsers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsPermissionManaged",
+                table: "AspNetUsers");
+        }
+    }
+}
