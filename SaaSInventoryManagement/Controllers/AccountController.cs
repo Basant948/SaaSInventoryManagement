32d5def0087543256c60f51b708b9c0f9101fd4e
basanti <basant123bsnt@gmail.com>
2026-07-20 09:46:30 +0545

    fix(auth): allow anonymous access  access denied pages

--- SaaSInventoryManagement/Controllers/AccountController.cs
+++ SaaSInventoryManagement/Controllers/AccountController.cs
@@ -63,6 +63,7 @@ namespace SaaSInventoryManagement.Controllers
             return RedirectToAction("Login", "Account");
         }
 
+        [AllowAnonymous]
         [HttpGet]
         public IActionResult AccessDenied() => View();
     }
