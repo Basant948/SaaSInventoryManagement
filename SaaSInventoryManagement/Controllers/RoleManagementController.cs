779151a774ea722a846ec08ecbf44fd696f3fdb7
basanti <basant123bsnt@gmail.com>
2026-07-20 07:54:26 +0545

    feat(controllers): add RoleManagementController gated by perm:inv.users.manage policy

---
+++ SaaSInventoryManagement/Controllers/RoleManagementController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SaaSInventoryManagement.Controllers
+{
+    [Authorize(Policy = "perm:inv.users.manage")]
+    public class RoleManagementController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
