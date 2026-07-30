5feeb072a5425186a291db265c4f16fa6b70af18
basanti <basant123bsnt@gmail.com>
2026-07-30 08:37:17 +0545

    feat: add generic repository, unit of work, and entity base contract

---
+++ SaaSInventoryManagement/Repositories/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using SaaSInventoryManagement.Models.Base;
+
+namespace SaaSInventoryManagement.Repositories.Interfaces
+{
+    public interface IUnitOfWork
+    {
+
+        IGenericRepository<TEntity> Repository<TEntity>() where TEntity : class, IEntity;
+
+        Task<int> SaveChangesAsync();
+    }
+}
---
+++ SaaSInventoryManagement/Repositories/UnitOfWork.cs
@@ -0,0 +1,29 @@
+﻿using SaaSInventoryManagement.Models.Base;
+using SaaSInventoryManagement.Repositories.Interfaces;
+
+namespace SaaSInventoryManagement.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly Dictionary<Type, object> _genericRepositories = new();
+
+        public UnitOfWork(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IGenericRepository<TEntity> Repository<TEntity>() where TEntity : class, IEntity
+        {
+            if (!_genericRepositories.TryGetValue(typeof(TEntity), out var repository))
+            {
+                repository = new GenericRepository<TEntity>(_db);
+                _genericRepositories[typeof(TEntity)] = repository;
+            }
+
+            return (IGenericRepository<TEntity>)repository;
+        }
+
+        public Task<int> SaveChangesAsync() => _db.SaveChangesAsync();
+    }
+}
